354324714f17ababe38a2f266a232a66e6761a39
rafaelsantos1000 <rafaasfs@gmail.com>
2022-06-30 08:53:33 +0000

    Create xpto page

--- DemoCet69.Web/Controllers/HomeController.cs
+++ DemoCet69.Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace DemoCet69.Web.Controllers
             return View();
         }
 
+        public IActionResult Xpto()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
